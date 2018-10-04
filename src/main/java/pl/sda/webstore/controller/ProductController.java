263dbd25feb6c8a11c8e2c91375576f66ded27b3
DESKTOP-95T3DT5\Michał Mielczarek <michal.mielczarek@gmail.com>
2018-10-04 11:21:43 +0200

    Dodane wyświetlanie produktu

---
+++ src/main/java/pl/sda/webstore/controller/ProductController.java
@@ -0,0 +1,26 @@
+package pl.sda.webstore.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.sda.webstore.domain.Product;
+
+@Controller
+public class ProductController {
+	
+	@RequestMapping("/products")
+	public String list(Model model){
+		Product xiaomiBetter = new Product("X1234","Redmi 4Pro", new BigDecimal(500));
+		xiaomiBetter.setDescription("Xiaomi Redmi 4Pro. Ekran 5 cali FULLHD. i takie tam. Xiaomi lepszy.");
+		xiaomiBetter.setCategory("Smart Phone");
+		xiaomiBetter.setManufacturer("Xiaomi");
+		xiaomiBetter.setUnitsInStock(665);
+		model.addAttribute("product", xiaomiBetter);
+		return "products";
+	}
+	
+
+}
